b03667696f831d453fd9a280d65c523521e89372
devps <devps@devps05.(none)>
2012-12-03 20:05:19 +0800

    letv

--- videoscrapy/spiders/movie_letv_spider.py
+++ videoscrapy/spiders/movie_letv_spider.py
@@ -14,7 +14,8 @@ class ShowsHaoSpider(BaseSpider):
     name = 'm_letv'
     allowed_domains = ['letv.com']
     start_urls = [
-        'http://so.letv.com/list/c1_t-1_a-1_y-1_f_at_o1_i-1_p.html',
+        #'http://so.letv.com/list/c1_t-1_a-1_y-1_f_at_o1_i-1_p.html',
+        'http://so.letv.com/list/c1_t8_a-1_y-1_f_at_o1_i-1_p.html',
     ]
   
     def __init__(self):
@@ -22,37 +23,45 @@ class ShowsHaoSpider(BaseSpider):
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select("//ol[@id='tabx_c_1' and @class='on']/div[@class='soyall']/dl[@class='info2_box']")
+        sites = hxs.select("//ol[@id='tabx_c_1' and @class='on']/div[@class='soyall']/div[@class='info2_box']")
 
         for site in sites:
             item = VideoItem()
             #item['area_name'] = re.sub("[\w\s]+","",site.select("a/@d-area").extract()[0]).replace("%","").replace("-","").strip() 
-            item['name'] = "".join(site.select("dt/a/@title").extract())
+            item['name'] = "".join(site.select("a/img/@alt").extract())
             #item['introduction'] = "".join(site.select("a/@d-intro").extract())
-            item['thumbnail'] = "".join(site.select("dt/a/img/@src").extract())
-            item['video_name'] = site.select("dt/a/@title").extract() 
-            item['video_thumbnail'] = site.select("dt/a/img/@src").extract() 
+            item['thumbnail'] = "".join(site.select("a/img/@src").extract())
+            item['video_name'] = site.select("a/img/@alt").extract() 
+            item['video_thumbnail'] = site.select("a/img/@src").extract() 
             #sec = "".join(re.sub("[\w\s]+","",site.select("a/@d-type").extract()[0])).replace("%","").replace("-","").strip()
             #item['sec_classify_name'] = sec.split(",") 
             #item['sec_classify'] = sec 
             #item['director'] = ",".join(site.select("div[@class='v-desc']/dl/dd[@class='v-s-actor']/a/text()").extract())
-            item['actors'] = ",".join(site.select("dd/a/text()").extract()) 
-            item['score'] = "".join(site.select("dd/span/i/text()").extract())
-           # item['publish_time'] = "" 
+            item['director'] = ",".join(site.select("dl[@class='info_dl']/dd[2]/a/text()").extract()) 
+            item['actors'] = ",".join(site.select("dl[@class='info_dl']/dd[3]/a/text()").extract()) 
+            item['score'] = "".join(site.select("dl[@class='info_dl']/dd[@class='tit']/span[@class='fr']/span[@class='sorce']/i/text()").extract())
+            item['publish_time'] = "".join(site.select("dl[@class='info_dl']/dd[@class='year_dl']/span/a/text()").extract()) 
             item['view_cnt'] = ""
             self.sort_index = self.sort_index + 1
             item["sort_index"] = self.sort_index
-            item['video_url'] = site.select("dt/a/@href").extract() 
-            detail_url = "".join(site.select("dt/a/@href").extract())
-            request = Request(detail_url,callback=self.parse_detail)
-            request.meta['item'] = item
+            item['video_url'] = site.select("dl[@class='info_dl']/dd[@class='btn_dd']/a/@href").extract() 
+            item['introduction'] = "".join(site.select("dl[@class='info_dl']/dd[@class='ind24']/text()").extract()).replace("\t","") 
+            item['video_introduction'] = [item['introduction']] 
+            item['video_sort_index'] = [1] 
+            item['video_view_cnt'] = [0] 
+            item['sec_classify'] = "".join(hxs.select("//a[@id='iq' and @class='on']/text()").extract()).replace("\t","") 
+            item['sec_classify_name'] = [item['sec_classify']] 
+            item['area_name']=u'\u5176\u4ed6'
+            #detail_url = "".join(site.select("dt/a/@href").extract())
+            #request = Request(detail_url,callback=self.parse_detail)
+            #request.meta['item'] = item
             #item['video_url'] = [] 
-            yield request 
-            #yield item 
+            #yield request 
+            yield item 
 
-        #for link in hxs.select("//div[@class='page']/a[last()]/@href").extract():
-        #    if link != '#'
-        #        yield Request(link,callback=self.parse)
+        for link in hxs.select("//div[@class='page']/a[last()]/@href").extract():
+            if link != '#':
+                yield Request(link,callback=self.parse)
 
 
     def parse_detail(self,response):
